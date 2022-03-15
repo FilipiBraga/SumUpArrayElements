a74af0559e5639f4652312400b73348c9085586d
filipibc <filipibc@gmail.com>
2022-03-15 14:58:20 -0300

    Implementando diversas formas para somar um array de inteiros

--- src/SumUpArrayElements/SumArrayElements.cs
+++ src/SumUpArrayElements/SumArrayElements.cs
@@ -2,5 +2,41 @@
 {
     public class SumArrayElements
     {
+        public int ForStatement(int[] sourceArray)
+        {
+            var result = 0;
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                result += sourceArray[i];
+            }
+
+            return result;
+        }
+
+        public int ForeachStatement(int[] sourceArray)
+        {
+            var result = 0;
+            foreach (var item in sourceArray)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+        public int EnumerableSum(int[] sourceArray)
+        {
+            return Enumerable.Sum(sourceArray);
+        }
+
+        public int ArraySum(int[] sourceArray)
+        {
+            return sourceArray.Sum();
+        }
+
+        public int Aggregate(int[] sourceArray)
+        {
+            return sourceArray.Aggregate((total, value) => total + value);
+        }
     }
 }
